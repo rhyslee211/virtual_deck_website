4d4bed49e5e70b150ac5a5f611b90c5c0e78a7e6
rhyslee211 <rhyslee211@gmail.com>
2025-01-14 14:43:56 -0500

    small fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import VirtualDeckLogo from "../virtualDeckLogo.png";
 const Home = () => {
     return (
     <div className="bg-gray-700 h-full flex-grow flex flex-col items-center justify-center">
-        <img src={VirtualDeckLogo} height={240} width={240} />
+        <img src={VirtualDeckLogo} height={240} width={240} alt="Homepage Logo" />
     </div>);
 };
 
